cdea7091b7734c53554583be866072d66a0299bc
Sarim Hasam <sarimhasanplace123@gmail.com>
2024-06-21 23:58:39 +0500

    add I don't know how many files

---
+++ src/components/NewsletterInput.jsx
@@ -0,0 +1,13 @@
+export default function NewsletterInput() {
+  return (
+    <div className="flex justify-center items-center">
+      <input type="email" name="" id="" className="w-[260px]" />
+      <button
+        className="text-tt-light text-[16px] border-2 border-black rounded-full"
+        type="submit"
+      >
+        Signup for my newsletter
+      </button>
+    </div>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: 'class', // Enable dark mode with the 'class' strategy
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EE9D4F',
+        'light-light': '#F5F5F5',
+        'light-dark': '#E7E7E7',
+        'tt-light': '#343A40',
+        'dark-light': '#4E555B',
+        'dark-dark': '#343A40',
+        'tt-dark': '#E7E7E7',
+      },
+    },
+  },
+  plugins: [],
+};
