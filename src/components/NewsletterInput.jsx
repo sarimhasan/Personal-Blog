4ed115e443a03dccaa9bafb75dd24c202571bf9c
Sarim Hasam <sarimhasanplace123@gmail.com>
2024-06-22 17:41:15 +0500

    add Hero section and make navbar responsive

--- src/components/NewsletterInput.jsx
+++ src/components/NewsletterInput.jsx
@@ -1,13 +1,23 @@
 export default function NewsletterInput() {
   return (
-    <div className="flex justify-center items-center">
-      <input type="email" name="" id="" className="w-[260px]" />
-      <button
-        className="text-tt-light text-[16px] border-2 border-black rounded-full"
-        type="submit"
-      >
-        Signup for my newsletter
-      </button>
+    <div className="flex justify-center">
+      <div>
+        <input
+          type="email"
+          name=""
+          id=""
+          className="w-[160px] md:w-[260px] py-1 px-2 rounded-l-full border-2 border-black outline-none"
+        />
+        <button
+          className="p-1 text-tt-light text-[16px] border-2 border-black rounded-r-full bg-primary md:w-[220px]"
+          type="submit"
+        >
+          Signup for my newsletter
+        </button>
+      </div>
     </div>
   );
 }
+
+// w-[260px] p-1 rounded-l-full border-2 border-black outline-none
+// text-tt-light text-[16px] border-2 border-black rounded-r-full p-1 bg-primary w-[220px]
