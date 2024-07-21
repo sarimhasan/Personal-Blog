fb09c3a63b947ff0a864ebb061d83d8a2a5689da
Sarim Hasam <sarimhasanplace123@gmail.com>
2024-07-21 18:28:13 +0500

    make some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,36 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
+import mdx from '@astrojs/mdx';
+
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: 'vitesse-dark',
+      // Alternatively, provide multiple themes
+      // See note below for using dual light/dark themes
+      // themes: {
+      //   light: 'github-light',
+      //   dark: 'github-dark',
+      // },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
   site: 'https://codereadsucceed.netlify.app/',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), expressiveCode(), mdx()],
   output: 'hybrid',
   adapter: netlify(),
 });
--- src/pages/blog/post-1.md
+++
@@ -1,8 +0,0 @@
----
-layout: ../../layouts/MarkdownPostLayout.astro
-title: Post 1
-description: This is post 1
-thumbnail: https://picsum.photos/id/237/200/300
----
-
-Hello bitches
